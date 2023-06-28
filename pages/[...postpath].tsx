a2b6a494c3b9d3313d6c4e87b0bf14707dabe251
Auto Click <autoclick.us@gmail.com>
2023-06-29 02:26:06 +0700

    rollback

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -25,9 +25,19 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	}
 	const query = gql`
 		{
-			post(id: "/${path}/", idType: URI) {
+			post(id: "/${path}", idType: URI) {
 				id
+				excerpt
 				title
+				link
+				dateGmt
+				modifiedGmt
+				content
+				author {
+					node {
+						name
+					}
+				}
 			}
 		}
 	`;
@@ -72,9 +82,26 @@ const Post: React.FC<PostProps> = (props) => {
 
 	return (
 		<>
-			
+			<Head>
+				<meta property="og:title" content={post.title} />
+				<link rel="canonical" href={`https://${host}/${path}`} />
+				<meta property="og:description" content={removeTags(post.excerpt)} />
+				<meta property="og:url" content={`https://${host}/${path}`} />
+				<meta property="og:type" content="article" />
+				<meta property="og:locale" content="en_US" />
+				<meta property="og:site_name" content={host.split('.')[0]} />
+				<meta property="article:published_time" content={post.dateGmt} />
+				<meta property="article:modified_time" content={post.modifiedGmt} />
+				<meta property="og:image" content={getImage(post.content)} />
+				<meta
+					property="og:image:alt"
+					content={post.title}
+				/>
+				<title>{post.title}</title>
+			</Head>
 			<div className="post-container">
-				<h1>Hello world</h1>
+				<h1>{post.title}</h1>
+				<article dangerouslySetInnerHTML={{ __html: post.content }} />
 			</div>
 		</>
 	);
